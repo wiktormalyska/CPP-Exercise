eceffad1bbb0a2fe5a3859dc09fbee90a8f0bfe7
Better Mobs <wikimal200310@gmail.com>
2023-01-16 19:21:13 +0100

    .

--- Kolos II/7/main.cpp
+++ Kolos II/7/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
-int silnia(int* n){
-    n = n+2;
-    return *n;
-}
-
 int main() {
-    int n = 10;
 
-    int *nPointer = &n;
-
-    int m = silnia(nPointer);
 
     return 0;
 }
